4e58967fff013e5e8124785300caa2a31dc9e842
nikos <athanikos@gmail.com>
2020-07-22 13:43:04 +0300

    circleci

--- setup_dev_user.py
+++ setup_dev_user.py
@@ -1,3 +1,3 @@
 from keyring import set_password
-set_password("CryptoCalculatorService", "USERNAME", "test")
-set_password("CryptoCalculatorService", "test", "test")
+set_password("CryptoCalculatorService", "USERNAME", "admin")
+set_password("CryptoCalculatorService", "admin", "admin")
