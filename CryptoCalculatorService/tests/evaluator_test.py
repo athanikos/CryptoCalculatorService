349aa56d24c8cac00be0c0bec355af4528ab7062
nikos <athanikos@gmail.com>
2020-08-06 21:04:31 +0300

    Scedhuler.py PricesService.py BalanceService.py PricesService_test.py badge circle ci

--- CryptoCalculatorService/tests/evaluator_test.py
+++ CryptoCalculatorService/tests/evaluator_test.py
@@ -1,14 +1,6 @@
-import datetime
-
-from cryptodataaccess.Rates.RatesMongoStore import RatesMongoStore
-from cryptodataaccess.helpers import do_connect
 import mock
 import pytest
-from CryptoCalculatorService.config import configure_app
-from cryptodataaccess.Rates.RatesRepository import RatesRepository
-from cryptomodel.coinmarket import prices
-from tests.helpers import insert_prices_record
-from CryptoCalculatorService.tests.helpers import mock_log, insert_exchange_record
+
 
 @pytest.fixture(scope='module')
 def mock_log():
