e518bef7dba7c088c088cf33547d97cdecee673b
nikos <athanikos@gmail.com>
2020-08-24 02:02:58 +0300

    Scedhuler.py PricesService.py BalanceService.py PricesService_test.py badge circle ci logs errors on exception in consumer write mongo check_every start_date end_date

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='CryptoCalculatorService-athanikos',
-    version="0.0.6",
+    version="0.0.7",
     license='MIT License',
     author='Nikos Athanasakis',
     packages=setuptools.find_packages(),
